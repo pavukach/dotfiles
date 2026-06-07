fcce02cbae0c59d99a6a66e5a09530417a891c3d
Maksym Diachok <maksdyachok2005@gmail.com>
2026-06-07 14:58:08 +0300

    Yazi keymaps update

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 let
-  make-keymap = on: command: { inherit on command; };
+  make-keymap = on: command: desc: { inherit on command desc; };
+  make-keymap-s = on: command: { inherit on command; };
   make-icon = name: text: { inherit name text; };
 in
 {
@@ -35,37 +36,37 @@ in
     keymap = {
       mgr = {
         prepend_keymap = [
-          (make-keymap "l" "plugin smart-enter")
-          (make-keymap "<C-y>" ''shell -- dragon-drop -x -i -T "$1"'')
-          (make-keymap "P" "shell -- wl-paste > pasted")
-          (make-keymap "y" [
+          (make-keymap-s "l" "plugin smart-enter")
+          (make-keymap-s "<C-y>" ''shell -- dragon-drop -x -i -T "$1"'')
+          (make-keymap-s "P" "shell -- wl-paste > pasted")
+          (make-keymap-s "y" [
             ''shell -- for path in "$@"; do echo "file://$path"; done | wl-copy -t text/uri-list''
             "yank"
           ])
-          (make-keymap "<C-o>" "shell -- setsid nemo . ")
-          (make-keymap "d" [
+          (make-keymap-s "<C-o>" "shell -- setsid nemo . ")
+          (make-keymap-s "d" [
             "escape --visual"
             ''shell -- trash-put "$@"''
           ])
-          (make-keymap "!" ''shell "$SHELL" --block'')
+          (make-keymap-s "!" ''shell "$SHELL" --block'')
 
-          (make-keymap [ "R" "o" ] "plugin recycle-bin -- open")
-          (make-keymap [ "R" "e" ] "plugin recycle-bin -- empty")
-          (make-keymap [ "R" "D" ] "plugin recycle-bin -- emptyDays")
-          (make-keymap [ "R" "d" ] "plugin recycle-bin -- delete")
-          (make-keymap [ "R" "r" ] "plugin recycle-bin -- restore")
+          (make-keymap [ "R" "o" ] "plugin recycle-bin -- open" "Open Bin")
+          (make-keymap [ "R" "e" ] "plugin recycle-bin -- empty" "Clear Bin")
+          (make-keymap [ "R" "D" ] "plugin recycle-bin -- emptyDays" "Clear Bin (days)")
+          (make-keymap [ "R" "d" ] "plugin recycle-bin -- delete" "Delete From Bin")
+          (make-keymap [ "R" "r" ] "plugin recycle-bin -- restore" "Restore File")
 
-          (make-keymap [ "c" "a" "a" ] "plugin compress")
-          (make-keymap [ "c" "a" "p" ] "plugin compress -p")
-          (make-keymap [ "c" "a" "h" ] "plugin compress -ph")
-          (make-keymap [ "c" "a" "l" ] "plugin compress -l")
-          (make-keymap [ "c" "a" "u" ] "plugin compress -phl")
+          (make-keymap [ "c" "a" "a" ] "plugin compress" "Compress")
+          (make-keymap [ "c" "a" "p" ] "plugin compress -p" "Compress (password)")
+          (make-keymap [ "c" "a" "h" ] "plugin compress -ph" "Compress (pass+header)")
+          (make-keymap [ "c" "a" "l" ] "plugin compress -l" "Compress (level)")
+          (make-keymap [ "c" "a" "u" ] "plugin compress -phl" "Compress (pass+header+level)")
         ];
       };
       pick = {
         prepend_keymap = [
-          (make-keymap "<C-n>" "arrow next")
-          (make-keymap "<C-p>" "arrow prev")
+          (make-keymap-s "<C-n>" "arrow next")
+          (make-keymap-s "<C-p>" "arrow prev")
         ];
       };
     };
